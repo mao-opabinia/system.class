f315f179326814d25e18aaef28c08fa7beef5096
mao-opabinia <t236019@st.yamagata-u.ac.jp>
2024-10-11 19:10:21 +0900

    Create centraldogma.c

---
+++ centraldogma.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+
+int main()
+{
+    printf("茶々丸-");
+    
+    int centraldogma;
+    centraldogma = 666;
+    
+    printf("Centraldogma is: %d",centraldogma);
+    
+    return 0;
+}
