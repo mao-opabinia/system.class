af0f3dda336907b96ac0f80c1a24e5c468b28cb4
mao-opabinia <t236019@st.yamagata-u.ac.jp>
2024-11-08 19:13:24 +0900

    Add files via upload

---
+++ 11.8課題3.c
@@ -0,0 +1,18 @@
+int main()
+{
+    int k;
+    printf("How many friends?");
+    scanf("%d",&k);
+    if (k>=100)
+    {
+        printf("There are many friends!");
+    }
+    else
+    {
+        printf("There are few friends!");
+    }
+    
+    
+    return 0;
+    
+}
